aa18960298953c12554d4978c5a97787edfc67dc
jeremy ku-benjet <jeremykubenjetschool@gmail.com>
2025-01-10 02:08:29 -0500

    Add tests.

--- test/test_tomlc99.m
+++ test/test_tomlc99.m
@@ -18,13 +18,20 @@
 
 :- import_module tomlc99.
 :- import_module maybe.
+:- import_module string.
 
 %----------------------------------------------------------------------------%
 
-main(!IO) :-
-  toml_parse_file("does_not_exit.toml", Res, !IO),
+:- pred test_toml_parse_file(string::in, io::di, io::uo) is det.
+test_toml_parse_file(Path, !IO) :-
+  toml_parse_file(Path, Res, !IO),
   (
-    Res = ok(_), S = "passed";
+    Res = ok(_), S = "parse successful";
     Res = error(S)
   ),
-  io.write_string(S, !IO).
+  io.write_string(S ++ "\n", !IO).
+  
+
+main(!IO) :-
+  test_toml_parse_file("does_not_exist.toml", !IO),
+  test_toml_parse_file("data/simple.toml", !IO).
