b60089eef3d22a0f0c9594cad276c300088ef5aa
jeremy ku-benjet <jeremykubenjetschool@gmail.com>
2025-02-11 00:10:21 -0500

    Add fragment model.

--- src/fragment.m
+++ src/fragment.m
@@ -15,8 +15,36 @@
 
 :- implementation.
 
+:- import_module map.
+:- import_module list.
+
 %----------------------------------------------------------------------------%
 
-:- type fragment ---> unit.
+:- type value --->
+  repeating(list(string));
+  single(string).
+
+:- type substitution --->
+  mul(
+    % The diliminator to separate repeating variables.
+    delim :: string,
+    % The name of the variable to replace.
+    label :: string
+  );
+
+  % The name of the variable to replace.
+  one(key :: string).
+
+
+:- type text_piece --->
+  lit(string);
+  subst(substitution).
+
+
+:- type fragment ---> 
+  fragment(
+    src :: list(text_piece),
+    vars :: map(string, value)
+  ).
 
 %----------------------------------------------------------------------------%
